2035baacba4515c5a4659938bb5b36ddd5ea479d
Dulani Lamahewage <95127130+lamahewageDulani@users.noreply.github.com>
2023-11-17 17:38:26 +0530

    Initial commit

---
+++ app/src/main/java/com/dr_lama/tha_app_204114m/utils/exceptions/AlertExceptions.java
@@ -0,0 +1,7 @@
+package com.dr_lama.tha_app_204114m.utils.exceptions;
+
+public class AlertExceptions extends Exception{
+    public AlertExceptions(String message){
+        super(message);
+    }
+}
